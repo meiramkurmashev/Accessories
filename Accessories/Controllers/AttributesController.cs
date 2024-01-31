2faf00630a7d15ebb25d2080f6900531de526e6e
Admin <meiramkurmashev@gmail.com>
2024-01-31 17:29:59 +0600

    new commit

--- Accessories/Controllers/AttributesController.cs
+++ Accessories/Controllers/AttributesController.cs
@@ -27,5 +27,47 @@ namespace Accessories.Controllers
             return View();
 
         }
+        public ActionResult Change(int id)
+        {
+            Attributes attributes = new Attributes();
+
+            List<Three> listAttributes = new List<Three>();
+
+
+            attributes.Att(ref listAttributes, id);
+
+            ViewBag.listAttributes = listAttributes;
+            ViewBag.id = id;
+
+            return View();
+
+        }
+        public ActionResult ChangeAtt(int id)
+        {
+            Attributes attributes = new Attributes();
+
+            List<Two> listAttributes = new List<Two>();
+
+
+            attributes.AttChange(ref listAttributes, id);
+
+            ViewBag.listAttributes = listAttributes;
+            ViewBag.id = id;
+
+            return View();
+
+        }
+        public ActionResult Delete(int id)
+        {
+            Attributes attributes = new Attributes();
+
+            
+
+            attributes.Delete(id);
+
+            return View();
+
+        }
+    
     }
 }
--- Accessories/Models/Attributes.cs
+++ Accessories/Models/Attributes.cs
@@ -25,7 +25,44 @@ namespace Accessories.Models
                             ID,
                             ATTRIBUTE_NAME,
                             CATEGORY_ID
-                            FROM attributes where category_Id ="+id_+" ";
+                            FROM attributes where category_Id =" + id_ + " ";
+
+            // объект для выполнения SQL-запроса
+            MySql.Data.MySqlClient.MySqlCommand command = new MySql.Data.MySqlClient.MySqlCommand(sql, conn);
+            // выполняем запрос и получаем ответ
+            using (MySql.Data.MySqlClient.MySqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    int id = (int)reader["id"];
+                    string name = (string)reader["ATTRIBUTE_NAME"];
+
+                    int category_Id = (int)reader["category_Id"];
+
+                    listAttributes.Add(new Three(id, name, category_Id));
+
+                }
+            }
+
+
+
+
+            conn.Close();
+
+        }
+        public void AttChange(ref List<Two> listAttributes, int id_)
+        {
+
+
+            MySql.Data.MySqlClient.MySqlConnection conn = new MySql.Data.MySqlClient.MySqlConnection(connStr);
+            // устанавливаем соединение с БД
+            conn.Open();
+            // запрос
+            string sql = @"SELECT
+                            ID,
+                            ATTRIBUTE_NAME,
+                            CATEGORY_ID
+                            FROM attributes where id ="+id_+" ";
         
             // объект для выполнения SQL-запроса
             MySql.Data.MySqlClient.MySqlCommand command = new MySql.Data.MySqlClient.MySqlCommand(sql, conn);
@@ -39,7 +76,7 @@ namespace Accessories.Models
                     
                     int category_Id = (int)reader["category_Id"];
 
-                    listAttributes.Add(new Three(id, name,category_Id));
+                    listAttributes.Add(new Two(id, name));
 
                 }
             }
@@ -70,5 +107,26 @@ namespace Accessories.Models
             conn.Close();
 
         }
+        public void Delete(int category_id)
+        {
+
+
+            MySql.Data.MySqlClient.MySqlConnection conn = new MySql.Data.MySqlClient.MySqlConnection(connStr);
+            
+            conn.Open();
+            
+            string sql = @"DELETE FROM ATTRIBUTES WHERE id = " + category_id;
+            
+            MySql.Data.MySqlClient.MySqlCommand command = new MySql.Data.MySqlClient.MySqlCommand(sql, conn);
+           
+            command.ExecuteReader();
+
+
+
+            conn.Close();
+
+        }
+
+      
     }
 }
